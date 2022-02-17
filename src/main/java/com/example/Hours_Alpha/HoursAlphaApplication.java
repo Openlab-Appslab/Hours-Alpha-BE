7c8defd17b41dc7474843ad4dbcc3339ae43e106
Martinekk <martinmatostano@gmail.com>
2022-02-17 11:37:31 +0100

    Add database

--- src/main/java/com/example/Hours_Alpha/HoursAlphaApplication.java
+++ src/main/java/com/example/Hours_Alpha/HoursAlphaApplication.java
@@ -9,11 +9,6 @@ public class HoursAlphaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HoursAlphaApplication.class, args);
 		System.out.println("Hello World!");
-		int a = 1;
-		int b = 2;
-		System.out.println(a+b);
-		System.out.println();
-		System.out.println("Hello World!");
 	}
 
 }
