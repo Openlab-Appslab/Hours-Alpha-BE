a766a51d99637439ddc5633d8f3f5ab27a24b600
Martinekk <martinmatostano@gmail.com>
2022-02-17 11:17:43 +0100

    Add database

---
+++ src/main/java/com/example/Hours_Alpha/HoursAlphaApplication.java
@@ -0,0 +1,14 @@
+package com.example.Hours_Alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoursAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoursAlphaApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
