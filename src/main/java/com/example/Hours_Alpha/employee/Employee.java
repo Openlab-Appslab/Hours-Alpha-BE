f8ab8028a272c9aad73a7a7fb0e7c2e04c24e8f4
Martinekk <martinmatostano@gmail.com>
2022-02-21 21:39:39 +0100

    add model of hour and Employee

--- src/main/java/com/example/Hours_Alpha/employee/Employee.java
+++ src/main/java/com/example/Hours_Alpha/employee/Employee.java
@@ -1,5 +1,24 @@
 package com.example.Hours_Alpha.employee;
 
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(
+        name = "employee"
+)
 public class Employee {
 
+    @Id
+    @SequenceGenerator(
+            name="employee_sequence",
+            sequenceName = "employee_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "employee_sequence"
+    )
+    private Long id;
 }
