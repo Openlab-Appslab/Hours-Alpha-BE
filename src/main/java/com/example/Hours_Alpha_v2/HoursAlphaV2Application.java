3c9cca3dc5421ffbeb6641790fe54bd5a4a2e597
Martinekk <martinmatostano@gmail.com>
2022-03-03 19:56:39 +0100

    First commit

---
+++ src/main/java/com/example/Hours_Alpha_v2/HoursAlphaV2Application.java
@@ -0,0 +1,13 @@
+package com.example.Hours_Alpha_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoursAlphaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoursAlphaV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hours_Alpha_v2/HoursAlphaV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hours_Alpha_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoursAlphaV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
