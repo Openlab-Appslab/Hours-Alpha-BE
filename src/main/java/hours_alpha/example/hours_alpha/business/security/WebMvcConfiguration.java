8acd1b645f887e9bce6ff3004e5fef7461f86a50
Martinekk <martinmatostano@gmail.com>
2022-03-31 19:23:10 +0200

    add cros allow

--- src/main/java/hours_alpha/example/hours_alpha/business/security/WebMvcConfiguration.java
+++ src/main/java/hours_alpha/example/hours_alpha/business/security/WebMvcConfiguration.java
@@ -10,7 +10,9 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200");
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 
 }
