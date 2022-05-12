c7e87a36906bc3d339bafb5ff62bba26d6050843
Martinekk <martinmatostanogmail.com>
2022-05-12 09:19:38 +0200

    ...

--- src/main/java/hours_alpha/example/hours_alpha/business/employee/dto/EmployeeGetBasicInfoDTO.java
+++ src/main/java/hours_alpha/example/hours_alpha/business/employee/dto/EmployeeGetBasicInfoDTO.java
@@ -2,6 +2,7 @@ package hours_alpha.example.hours_alpha.business.employee.dto;
 
 import hours_alpha.example.hours_alpha.business.company.Company;
 import hours_alpha.example.hours_alpha.business.hour.Hour;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeGetBasicInfoDTO {
     private String email;
     private String firstName;
