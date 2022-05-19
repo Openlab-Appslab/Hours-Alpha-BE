b9fbd75d23ea756a3cbee4a1c1995766c5189642
Martinekk <martinmatostanogmail.com>
2022-05-19 08:34:25 +0200

    add DTO of company basic info and endpoint for that

---
+++ src/main/java/hours_alpha/example/hours_alpha/exception/PresentVariableIsNull.java
@@ -0,0 +1,7 @@
+package hours_alpha.example.hours_alpha.exception;
+
+public class PresentVariableIsNull extends RuntimeException{
+    public PresentVariableIsNull(String message){
+        super(message);
+    }
+}
